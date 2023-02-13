8730b1b2a652c43b1ba23f884a29d7868b56970e
Anas Obaid <anasobaidpk@gmail.com>
2023-02-13 23:25:55 +0500

    Setup home (ride) page.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+const style = {
+  wrapper: `h-screen w-screen flex flex-col`,
+}
+
 export default function Home() {
-  return 
+  return (
+    <div className={ style.wrapper }>
+      {/* <Navbar /> */}
+      <div className={ style.main }>
+        <div className={ style.mapContainer }>
+          {/* <Map /> */}
+        </div>
+      </div>
+      <div className={ style.rideRequestContainer }>
+        <div className={ style.rideRequest }>
+          {/* <LocationSelector /> */}
+          {/* <Confirm /> */}
+        </div>
+      </div>
+    </div>
+  )
 }
