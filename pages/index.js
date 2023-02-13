a797e6290319118c279fef3a73d6b82b198124ed
Anas Obaid <anasobaidpk@gmail.com>
2023-02-13 23:41:29 +0500

    Build location selector component.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Navbar from "@/components/Navbar"
 import Map from "@/components/Map"
+import LocationSelector from "@/components/LocationSelector"
 
 
 const style = {
   wrapper: `h-screen w-screen flex flex-col`,
   main: `h-full w-screen flex-1 z-10 overflow-hidden`,
   mapContainer: `flex-1 w-full h-full`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[700px] bg-white shadow-lg rounded-lg flex flex-col overflow-scroll`,
 }
 
 export default function Home() {
@@ -19,7 +22,7 @@ export default function Home() {
       </div>
       <div className={ style.rideRequestContainer }>
         <div className={ style.rideRequest }>
-          {/* <LocationSelector /> */}
+          <LocationSelector />
           {/* <Confirm /> */}
         </div>
       </div>
