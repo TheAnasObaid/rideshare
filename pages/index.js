38fbe361d24a5ac2603ca2b32eb46421c259aac2
Anas Obaid <anasobaidpk@gmail.com>
2023-02-13 23:36:34 +0500

    Build the map component.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Navbar from "@/components/Navbar"
+import Map from "@/components/Map"
 
 
 const style = {
   wrapper: `h-screen w-screen flex flex-col`,
+  main: `h-full w-screen flex-1 z-10 overflow-hidden`,
+  mapContainer: `flex-1 w-full h-full`,
 }
 
 export default function Home() {
@@ -11,7 +14,7 @@ export default function Home() {
       <Navbar />
       <div className={ style.main }>
         <div className={ style.mapContainer }>
-          {/* <Map /> */}
+          <Map />
         </div>
       </div>
       <div className={ style.rideRequestContainer }>
