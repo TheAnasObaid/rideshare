1a1457348ec3519ff47b5fd841b5ad45bf016c5e
Anas Obaid <anasobaidpk@gmail.com>
2023-02-14 23:10:08 +0500

    Fix sanity not working.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+          content: [
+        "./pages/**/*.{html,js,ts,jsx,tsx}",
+        "./components/**/*.{html,js,ts,jsx,tsx}",
+        // "./app/**/*.{html,js,ts,jsx,tsx}",
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    },
     autoprefixer: {},
   },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{html,js,ts,jsx,tsx}",
     "./components/**/*.{html,js,ts,jsx,tsx}",
+    "./app/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
