f145b18b26127181248d7e2fee4942f4cf610bc2
Anas Obaid <anasobaidpk@gmail.com>
2023-05-15 08:08:47 +0500

    Did some more changes in the code.

--- components/user/Confirm.js
+++ components/user/Confirm.js
@@ -1,13 +1,17 @@
 import RideSelector from "./RideSelector";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ethers } from "ethers";
 import { RideContext } from "@/context/rideContext";
+
 
 const style = {
     wrapper: `flex-1 h-full flex flex-col justify-between overflow-auto`,
     rideSelectorContainer: `h-full flex flex-col overflow-auto`,
     confirmButtonContainer: ` border-t-2 z-10`,
     confirmButton: `bg-black text-white cursor-pointer m-4 py-4 text-center text-xl`,
+    modalContainer: `mt-10 flex flex-col justify-center items-center w-72 rounded-lg shadow-xl h-auto p-2`,
+    modalHeading: `text-base mt-2 mx-4 text-gray-400 font-semibold text-center`,
+    modalButton: `my-5 w-auto px-8 h-10 bg-blue-600 text-white rounded-md shadow hover:shadow-lg font-semibold`
 };
 
 const Confirm = () => {
@@ -63,12 +67,13 @@ const Confirm = () => {
                 <div className={style.confirmButtonContainer}>
                     <div
                         className={style.confirmButton}
-                        onClick={() => storeTripDetails(pickup, dropoff)}
+                        onClick={() => setShowModal(true)}
                     >
                         Confirm {selectedRide.service || "RideX"}
                     </div>
                 </div>
             </div>
+            
         </div>
     );
 };
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,12 @@
 import HeaderDriver from "@/components/driver/HeaderDriver"
 import NavbarDriver from "@/components/driver/NavbarDriver"
 
+
 export default function Dashboard() {
     return (
         <div>
             <HeaderDriver />
             <NavbarDriver />
-            {/* Content */}
         </div>
     )
 }
