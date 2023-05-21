23d9c443bf846f0f499860b791362730356291f8
Anas Obaid <anasobaidpk@gmail.com>
2023-05-21 19:57:17 +0500

    Renamed the Header.

---
+++ components/Header.js
@@ -0,0 +1,40 @@
+import { RideContext } from "@/context/rideContext";
+import { useContext } from "react";
+import Link from "next/link";
+
+const style = {
+  wrapper: `h-16 w-full bg-white text-black flex justify-between items-center px-16 z-20`,
+  logo: `text-3xl text-black font-medium flex mr-16`,
+  menuItem: `text-lg text-black font-medium flex items-center mx-4 cursor-pointer`,
+  connectButton: `text-lg text-black font-medium cursor-pointer ml-2 hover:bg-gray-100 p-3 rounded-md`,
+};
+
+const Header = () => {
+  const { currentAccount, connectWallet, currentUser } =
+    useContext(RideContext);
+
+  return (
+    <div className={style.wrapper}>
+      <Link className={style.logo} href="/">
+        RideShare
+      </Link>
+      {currentAccount ? (
+        <div className={style.connectButton}>{currentUser.name}</div>
+      ) : (
+        <div className={style.connectButton}> </div>
+      )}
+      {currentAccount ? (
+        <div className={style.connectButton}>
+          {currentAccount.slice(0, 5)}....
+          {currentAccount.slice(39)}
+        </div>
+      ) : (
+        <div className={style.connectButton} onClick={() => connectWallet()}>
+          Connect Wallet
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
